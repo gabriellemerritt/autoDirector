602019d378e750ea7c19db33d84820c2c5df085c
Gabrielle Merritt <gabrielle.merritt13@gmail.com>
2016-01-22 23:48:39 -0500

    adding houndify api

---
+++ houndify-sdk-sample-0.2.16/src/main/java/com/houndify/sample/Constants.java
@@ -0,0 +1,9 @@
+package com.houndify.sample;
+
+/**
+ * General constants.
+ */
+public class Constants {
+    public static final String CLIENT_ID = "client_id_goes_here";
+    public static final String CLIENT_KEY = "client_key_goes_here";
+}
